7691c2a6c14aea09a6fffaa8857b78fec7927b59
mancunian1792 <linkinnation1792@gmail.com>
2016-01-31 01:16:57 +0530

    Changes to File
    Changed cache matrix file in align to the week 3 changes

--- Week 3/ProgrammingAssignment2-master/cachematrix.R
+++ Week 3/ProgrammingAssignment2-master/cachematrix.R
@@ -3,13 +3,48 @@
 
 ## Write a short comment describing this function
 
+## The function is used to cache the valuue of the matrix
+## and there are four functions created which is used to set,
+## get ,set the inverse and get the inverse.
+
 makeCacheMatrix <- function(x = matrix()) {
+
+m<-null
+set <- function(y) {
+                x <<- y
+       
+         m <<- NULL
+        }
+get <- function() x
+setinv <- function(inv) m <<- inv
+getinv<-function() m
+list(set=set,get=get,setinv=setinv,getinv=getinv)
 
 }
 
 
-## Write a short comment describing this function
+## Write a short comment describing this function 
+
+## This function takes in a matrix and attempts to solve the simultaneous equations
+## by getting hold of the inverse of  the matrix.Now ,this inverse can be a cached value.
+## If not,use the solve method to compute the inverse.To do this,we get the matrix(which again can be cached)
+## and use solve and set the inverse
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+m<-x$getinv()
+if(!is.null(m)){
+
+	message("getting cached inverse matrix")
+	return(m)
+
+
+}
+
+
+mat<-x$get
+m<-solve(mat)
+
+x$setinv(m)
 }
